c42ed2754b344b2e7abedd31011bdf01cdc3bc53
Ryan <coulson.ryan@gmail.com>
2021-07-09 12:21:39 +0200

    add navbar and links

--- script.js
+++ script.js
@@ -24,27 +24,27 @@ let letter = '';
   setTimeout(type, 200);
 })();
 
-let load = 0;
+// let load = 0;
 
-let int = setInterval(blurring, 30);
+// let int = setInterval(blurring, 30);
 
-function blurring() {
-  const loadText = document.querySelector('.loading-text');
-  const bg = document.querySelector('.bg');
-  const intro = document.querySelector('.intro');
-  const navbar = document.querySelector('.navbar');
-  load++;
+// function blurring() {
+//   const loadText = document.querySelector('.loading-text');
+//   const bg = document.querySelector('.bg');
+//   const intro = document.querySelector('.intro');
+//   const navbar = document.querySelector('.navbar');
+//   load++;
 
-  if (load > 99) {
-    clearInterval(int);
-  }
-  loadText.innerText = `${load}%`;
-  loadText.style.opacity = scale(load, 0, 100, 1, 0);
-  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
-  intro.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
-  navbar.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
-}
+//   if (load > 99) {
+//     clearInterval(int);
+//   }
+//   loadText.innerText = `${load}%`;
+//   loadText.style.opacity = scale(load, 0, 100, 1, 0);
+//   bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+//   intro.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+//   navbar.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+// }
 
-const scale = (num, in_min, in_max, out_min, out_max) => {
-  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
-};
+// const scale = (num, in_min, in_max, out_min, out_max) => {
+//   return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+// };
